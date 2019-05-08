a66bf16125e0059f57dceb368555412a14777619
novassy <mina37@gmail.com>
2019-05-08 22:13:58 +0900

    やるぞ

--- janken.php
+++ janken.php
@@ -10,45 +10,51 @@
 	//もし　プレイヤーが0　でコンピュータが2　なら
 	//「負けです」と表示する
 	//以下なんかそんなかんじ
+	do {
+		echo "手を選んでください(0=グー, 1=チョキ, 2=パーを入力)".PHP_EOL;
+		//相手の入力および手の表示
+		$hand = ['グー', 'チョキ', 'パー'];
+		if(isset($_GET['player'])){
+			$player = $_GET['player'];
+		}
+		$playernum = intval($player);
+		echo "あなた「".$hand[$playernum]."」\n";
+		//こちらの手の選択および表示
+		$rand = array_rand($hand);
+		echo "相手「".$hand[$rand]."！」\n";
+		$comp = intval($rand);
+
+		//グーのとき
+		if ($player == 0 && $comp == 0) {
+		echo "あいこです！もう一度\n".PHP_EOL;
+		}
+		if ($player == 0 && $comp == 1) {
+		echo "あなたの勝ちです！".PHP_EOL;
+		}
+		if ($player == 0 && $comp == 2) {
+		echo "あなたの負けです……".PHP_EOL;
+		}
 	
-	echo "手を選んでください(0=グー, 1=チョキ, 2=パーを入力)".PHP_EOL;
-	$hand = ['グー', 'チョキ', 'パー'];
-	$player = fgets(STDIN);
-	$playernum = intval($player);
-	echo "あなた「".$hand[$playernum]."」\n";
-	$rand = array_rand($hand);
-	echo "相手「".$hand[$rand]."！」\n";
-	//グーのとき
-	if ($player == 0 && $rand == 0) {
-	echo "あいこです".PHP_EOL;
-	}
-	if ($player == 0 && $rand == 1) {
-	echo "勝ちです".PHP_EOL;
-	}
-	if ($player == 0 && $rand == 2) {
-	echo "負けです".PHP_EOL;
-	}
-
-	//チョキのとき
-	if ($player == 1 && $rand == 1) {
-	echo "あいこです".PHP_EOL;
-	}
-	if ($player == 1 && $rand == 2) {
-	echo "勝ちです".PHP_EOL;
-	}
-	if ($player == 1 && $rand == 0) {
-	echo "負けです".PHP_EOL;
-	}
-
-	//パーのとき
-	if ($player == 2 && $rand == 2) {
-	echo "あいこです".PHP_EOL;
-	}
-	if ($player == 2 && $rand == 0) {
-	echo "勝ちです".PHP_EOL;
-	}
-	if ($player == 2 && $rand == 1) {
-	echo "負けです".PHP_EOL;
-	}
-
+		//チョキのとき
+		if ($player == 1 && $comp == 1) {
+		echo "あいこです！もう一度\n".PHP_EOL;
+		}
+		if ($player == 1 && $comp == 2) {
+		echo "あなたの勝ちです！".PHP_EOL;
+		}
+		if ($player == 1 && $comp == 0) {
+		echo "あなたの負けです……".PHP_EOL;
+		}
+	
+		//パーのとき
+		if ($player == 2 && $comp == 2) {
+		echo "あいこです！もう一度\n".PHP_EOL;
+		}
+		if ($player == 2 && $comp == 0) {
+		echo "あなたの勝ちです！".PHP_EOL;
+		}
+		if ($player == 2 && $comp == 1) {
+		echo "あなたの負けです……".PHP_EOL;
+		}
+	} while ($player == $comp);
 	
